ed08c963ae1ad85e4f21a9b1581f14e3931ce8d0
Skerdinajol <69467442+Skerdinajol@users.noreply.github.com>
2021-05-04 18:06:07 +0200

    bera ca gjera

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KembimValutor.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult register()
+        {
+            return View();
+        }
+        public ActionResult userdetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KembimValutor.Models
+{
+    public class users
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public float Wallet { get; set; }
+    }
+}
